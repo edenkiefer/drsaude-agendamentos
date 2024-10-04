469567899923a4abe36d5b4a6303d1b42f3274a0
Eden Kiefer <edenkieferdev@gmail.com>
2024-10-04 13:07:03 -0300

    feat: add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import { Outlet } from 'react-router-dom'
+
+import { Header } from './components/Header'
+import AppointmentsContextProvider from './contexts/AppointmentsContext'
+import { AppContainer } from './styles/global'
+
 function App() {
-  return <h1>Dr. Saúde - Agendamentos</h1>
+  return (
+    <AppointmentsContextProvider>
+      <Header />
+      <AppContainer>
+        <Outlet />
+      </AppContainer>
+    </AppointmentsContextProvider>
+  )
 }
 
 export default App
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,42 @@
 import { createBrowserRouter } from 'react-router-dom'
 
 import App from './App'
+import Appointment from './pages/Appointment'
+import { AppointmentCompleted } from './pages/AppointmentCompleted'
+import { Confirmation } from './pages/Confirmation'
+import { PatientRegistration } from './pages/PatientRegistration'
+import { Payment } from './pages/Payment'
+import { SignIn } from './pages/SignIn'
 
 export const router = createBrowserRouter([
   {
     path: '/',
     element: <App />,
+    children: [
+      {
+        path: '/',
+        element: <Appointment />,
+      },
+      {
+        path: '/sign-in',
+        element: <SignIn />,
+      },
+      {
+        path: '/patient-registration',
+        element: <PatientRegistration />,
+      },
+      {
+        path: '/confirmation',
+        element: <Confirmation />,
+      },
+      {
+        path: '/payment',
+        element: <Payment />,
+      },
+      {
+        path: '/appointment-completed',
+        element: <AppointmentCompleted />,
+      },
+    ],
   },
 ])
