2f6670a8aeb0fe34a71e5571aad8bc87039f3055
Eden Kiefer <edenkieferdev@gmail.com>
2024-10-07 10:42:46 -0300

    fix: fixing navigation problem in redirection

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -99,7 +99,7 @@ export function SignIn() {
           </form>
         </SignInContainer>
       ) : (
-        <Navigate to="/confirmation" />
+        <Navigate to="/confirmation" replace />
       )}
     </>
   )
